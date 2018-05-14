720e61c41b1c0dc035e4b8d20e96b32028b9db77
奶爸 <1@5.nu>
2018-05-14 10:32:54 +0800

    init front-end framework

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <router-view></router-view>
+
+
+    </div>
+</template>
+
+
+<script>
+
+    export default {
+        name: 'App',
+        data: function () {
+            return {}
+        },
+        methods: {},
+        components: {}
+    }
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <button class="button is-primary"> Hello {{msg}} !</button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'hello',
+        data: function () {
+            return {
+                msg: "Naiba"
+            }
+        },
+        methods: {},
+        components: {}
+    }
+</script>
+
+<style scoped>
+</style>
